de6fda4676fc3dea209bbbcdb4916088cd437648
adamrturman <adamrturman@gmail.com>
2020-12-11 21:02:09 -0600

    initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const height = ($('#hero').height() - $('#sticky').height())
+
+$(window).scroll(function() {
+    if($(this).scrollTop() >= height) {
+        $('#sticky').addClass('fixed-top');
+        $('#sticky').addClass('bg-white');
+    } else {
+        $('#sticky').removeClass('fixed-top');
+    }
+});
